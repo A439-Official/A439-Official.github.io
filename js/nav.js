d08ef24e30033fa8dce4afbe18be719074cc3795
A439 <18359153400@163.com>
2024-12-11 16:15:42 +0800

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -2,18 +2,24 @@ var nav = document.createElement("div");
 nav.setAttribute("class", "nav");
 
 var logo = document.createElement("img");
-logo.setAttribute("src", "https://A439-OwO.github.io/img/logo.png");
-logo.setAttribute("alt", "logo");
+logo.setAttribute("src", "https://a439-owo.github.io/img/A439.png");
+var myaccount = document.createElement("a");
+myaccount.setAttribute("class", "logo");
+myaccount.setAttribute("href", "https://space.bilibili.com/3493079245916811");
+myaccount.setAttribute("target", "_blank");
+myaccount.appendChild(logo);
+nav.appendChild(myaccount);
 
 var ul = document.createElement("ul");
+ul.setAttribute("class", "linklist");
 
 var li1 = document.createElement("li");
 var a1 = document.createElement("a");
-a1.setAttribute("href", "#");
+a1.setAttribute("href", "https://a439-owo.github.io/");
 a1.appendChild(document.createTextNode("Home"));
 li1.appendChild(a1);
-
 ul.appendChild(li1);
+
 nav.appendChild(ul);
 
 var header = document.getElementsByClassName("header")[0];
