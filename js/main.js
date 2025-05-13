90dc5f60fb823440069efd6a1b50ec71333b0b46
A439 <18359153400@163.com>
2025-05-13 15:52:56 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -42,3 +42,7 @@ var favicon = document.createElement("link");
 favicon.setAttribute("rel", "icon");
 favicon.setAttribute("href", "/img/AF.png");
 document.head.appendChild(favicon);
+
+var script = document.createElement('script');
+script.src = '/js/mouse.js';
+document.head.appendChild(script);
